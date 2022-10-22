014366d6625830978eb9e4b5b307cb8abf4ae8df
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2022-10-23 00:49:03 +0530

    Time: 20 ms (72.23%), Space: 5.9 MB (74.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int i,j,n;
+        
+        if(x < 0){
+            return false;
+        }
+        
+        string str = to_string(x);
+        n  = str.size();
+        
+        i = 0, j = (n-1);
+        while(i < j){
+            if(str[i] != str[j]){
+                return false;
+            }
+            
+            i++, j--;
+        }
+        
+        return true;                
+    }
+};
