3818a11b84f323619de4c68be484e41456c3bed9
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2022-11-04 19:46:06 +0530

    Time: 420 ms (90.31%), Space: 167.8 MB (82.62%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,37 +1,60 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> map;
-        for(auto word : words){
-            map[word] ++;
+        int i,n,cnt,ab_cnt,ba_cnt,len;
+        n = words.size();
+        
+        map<string,int> freq; // {word,cnt}
+        for(i=0 ; i<n ; i++){
+            freq[words[i]]++;
         }
-        int ans = 0;
-        for(int i=0; i<words.size(); i++) {
-            string str = words[i];
-            if(map[str] <= 0){
-                continue;
-            }
-            map[str] --;
-            string str_rev = str;
-            reverse(str_rev.begin(), str_rev.end());
-            if(map.find(str_rev) != map.end() and map[str_rev] > 0){
-                //cout<<str<<" ";
-                ans +=4;
-                map[str_rev] --;
-            }
-            else{
-                map[str] ++;
-            }
-        }
-        for(int i=0; i<words.size(); i++) {
-            string str = words[i];
-            if(map[str] > 0) {
-                if(str[0] == str[1]) {
-                    ans += 2;
-                    break;
+        
+        bool aa_form_oddOccurence_present = false;
+        len = 0;
+        for(auto x : freq){
+            string currentWord = x.first;            
+            if(currentWord[0] == currentWord[1]){
+                // aa form
+                cnt = x.second;
+                if(cnt%2 == 0){
+                    len += 2*(2*cnt);        //  extra 2* for double counting
+                }
+                else{
+                    len += 2*(2*(cnt - 1));  
+                    aa_form_oddOccurence_present = true;
                 }
             }
+            else{
+                // ab form                
+                string pairedWord;
+                pairedWord += currentWord[1];
+                pairedWord += currentWord[0];
+                
+                if(freq.find(pairedWord) != freq.end()){
+                    // Paired word (ba) is present
+                    ab_cnt = freq[currentWord];
+                    ba_cnt = freq[pairedWord];
+                    /*      Let, ab_cnt = 2 
+                                 ba_cnt = 3.
+                             
+                            Then, mn = 2
+                        
+                            Thus Palindrome: ba-ba- - - - - - ab-ab
+                        
+                            len += (2*mn) + (2*mn)   // 4 + 4
+                         ie,len += 4*mn                                                 
+                    */
+                    len += 4*(min(ab_cnt,ba_cnt)); 
+                }                                                                
+            }
+            
+        }        
+        len /= 2;       // Counted both ab-ba pairs and ba-ab pairs
+        
+        if(aa_form_oddOccurence_present){
+            len += 2;
         }
-        return ans;
+        
+        return len;
     }
 };
