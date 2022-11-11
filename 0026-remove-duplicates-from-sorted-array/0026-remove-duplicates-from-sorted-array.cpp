751077656bbffb6c94052211b62cbbceb83854d1
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2022-11-12 00:18:49 +0530

    Time: 20 ms (71.10%), Space: 18.3 MB (74.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,18 +4,15 @@ public:
         int current,i,j,n;
         n = nums.size();
         
-        i = 0, j = 0;
-        while(j < n){
-            if(nums[j] != current){
-                nums[i] = nums[j];
-                i++;
-                
-                current = nums[j];
+        current = -1000, j = 0;
+        for(i=0 ; i<n ; i++){
+            if(nums[i] != current){
+                nums[j] = nums[i];
+                current = nums[i];
+                j++;
             }
-            
-            j++;
         }
         
-        return i;
+        return j;
     }
 };
