48c4bf90ec989aa6b12a18fb5cecc71884f1d363
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2022-12-29 10:41:44 +0530

    Time: 735 ms (77.78%), Space: 157.5 MB (20.90%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:    
+    vector<int> getOrder(vector<vector<int>>& V) {
+        int n,i,currentTime,burstTime,id;
+        n = V.size();
+        
+        vector<vector<int>> tasks(n);
+        for(i=0 ; i<n ; i++){
+            tasks[i] = {V[i][0],V[i][1],i};
+        }
+        
+        sort(tasks.begin(),tasks.end());
+        
+        set<pair<int,int>> P; // {burstTime,id} (available tasks)
+        vector<int> solution;        
+        currentTime = 0;
+        for(i=0 ; i<n ; ){            
+            if(P.size() == 0 && currentTime < tasks[i][0]){
+                currentTime = tasks[i][0];                
+            }
+                                        
+            while(i<n && tasks[i][0] <= currentTime){
+                P.insert({tasks[i][1],tasks[i][2]});
+                i++;
+            }
+            
+            // Choosing the task for processing
+            id = (*(P.begin())).second;
+            burstTime = (*(P.begin())).first;
+            P.erase(P.begin());
+            
+            // cout<<id<<" "<<"\n";
+            
+            solution.push_back(id);
+            currentTime = (currentTime + burstTime);
+        }
+                    
+        while(P.size() >= 1){
+            id = (*(P.begin())).second;
+            burstTime = (*(P.begin())).first;
+            P.erase(P.begin());            
+            solution.push_back(id);            
+        }
+        
+        return solution;        
+    }
+};
