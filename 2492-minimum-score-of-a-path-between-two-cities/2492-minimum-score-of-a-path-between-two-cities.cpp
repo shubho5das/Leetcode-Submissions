e8f44ea4017ef66a8f63866f1dcf1e3bc0a515cc
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2023-03-23 02:18:49 +0530

    Time: 800 ms (12.73%), Space: 194.9 MB (5.21%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -2,39 +2,35 @@
 
 class Solution {
 public:
-    void dfs(int src,vector<bool> &visited, vector<vector<int>> &adjList){
+    void dfs(int src,vector<bool> &visited, vector<vector<vector<int>>> &adjList,int &minEdge){
         visited[src] = true;
         
-        for(auto nbr : adjList[src]){            
-            if(!visited[nbr]){
-                dfs(nbr,visited,adjList);
+        for(auto nbr : adjList[src]){ 
+            int nbrNode = nbr[0];
+            int nbrWt = nbr[1];
+            
+            minEdge = min(minEdge, nbrWt);
+            
+            if(!visited[nbrNode]){                
+                dfs(nbrNode,visited,adjList,minEdge);
             }
         }
     }
     
     int minScore(int n, vector<vector<int>>& roads) {
-        int i,u,v,wt,minScore;
+        int i,u,v,wt,minEdge;
         
-        vector<vector<int>> adjList(n+1);
-        for(i=0 ; i<roads.size() ; i++){
-            u = roads[i][0], v = roads[i][1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
-        }
-        
-        // 1
-        vector<bool> visited(n+1,false);
-        dfs(1,visited,adjList);
-           
-        // 2
-        minScore = inf;
+        vector<vector<vector<int>>> adjList(n+1);
         for(i=0 ; i<roads.size() ; i++){
             u = roads[i][0], v = roads[i][1], wt = roads[i][2];
-            if(visited[u] && visited[v]){
-                minScore = min(minScore,wt);
-            }
+            adjList[u].push_back({v,wt});    
+            adjList[v].push_back({u,wt});    
         }
+       
+        vector<bool> visited(n+1,false);
+        minEdge = inf;
+        dfs(1,visited,adjList,minEdge);
         
-        return minScore;     
+        return minEdge;
     }
 };
