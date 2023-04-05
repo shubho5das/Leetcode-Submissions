ea6b9ff455f1b92702ab4654090cb887a3a6cbe5
Shubho Das <60283224+shubho5das@users.noreply.github.com>
2023-04-05 21:57:09 +0530

    Time: 832 ms (60.54%), Space: 26 MB (84.30%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        area = 0
+        best = 0
+        for i in range(N):
+            area += nums[i]
+
+            best = max(best, (area + i) // (i + 1))
+        return best
+
+                
